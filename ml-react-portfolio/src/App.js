7c08f3f1474eeb3413f8d940297944eadc895541
Mark <markloercher@gmail.com>
2022-02-25 14:44:29 -0500

    added renderMain function

--- ml-react-portfolio/src/App.js
+++ ml-react-portfolio/src/App.js
@@ -1,23 +1,44 @@
 import React, { useState } from "react";
-import About from './components/About';
 // import components below 
+import About from './components/About';
 import Nav from './components/Nav';
 import Projects from './components/Projects'
 import Contact from "./components/Contact/Contact";
 
 function App() {
 
+  const [pageRendered, setPageRendered] = useState("Contact");
+
+  function renderMain () {
+
+    if(pageRendered === "About") {
+      return (
+            <About />
+        )
+    } else if (pageRendered === "Projects") {
+      return (
+        <Projects /> 
+      )
+    } else if (pageRendered === "Contact") {
+      return (
+        <Contact />
+      )
+    }
+
+  }
+
 
   return (
     <div>
       <Nav />
       <main>
-        <About />
-        <Projects />
-        <Contact />
+        {
+          renderMain()
+        }
       </main>
     </div>
   );
+
 }
 
 export default App;
--- ml-react-portfolio/src/components/Projects/index.js
+++ ml-react-portfolio/src/components/Projects/index.js
@@ -1,38 +1,64 @@
 import React, { useState } from "react";
-import projectImage from '../../assets/cover-image.jpg';
+import projectImage from "../../assets/cover-image.jpg";
 
-// add an array of objects containing project names and descriptions 
+// add an array of objects containing project names and descriptions
 // add hook to utilize useState to display project titles and images accordingly.
 
-function Projects(){
-    return(
-        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-        </ol>
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img class="d-block w-100" src={projectImage} alt="First slide"/>
-          </div>
-          <div class="carousel-item">
-            <img class="d-block w-100" src={projectImage} alt="Second slide"/>
-          </div>
-          <div class="carousel-item">
-            <img class="d-block w-100" src={projectImage} alt="Third slide"/>
+function Projects() {
+  return (
+    <div
+      id="carouselExampleIndicators"
+      class="carousel slide"
+      data-ride="carousel"
+    >
+      <ol class="carousel-indicators">
+        <li
+          data-target="#carouselExampleIndicators"
+          data-slide-to="0"
+          class="active"
+        ></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img class="d-block w-100" src={projectImage} alt="First slide" />
+        </div>
+        <div class="carousel-item">
+          <img class="d-block w-100" src={projectImage} alt="Second slide" />
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Proj 1</h5>
+            <p>This is the description of the proj</p>
           </div>
         </div>
-        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
+        <div class="carousel-item">
+          <img class="d-block w-100" src={projectImage} alt="Third slide" />
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Proj 1</h5>
+            <p>This is the description of the proj</p>
+          </div>
+        </div>
       </div>
-    )
+      <a
+        class="carousel-control-prev"
+        href="#carouselExampleIndicators"
+        role="button"
+        data-slide="prev"
+      >
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a
+        class="carousel-control-next"
+        href="#carouselExampleIndicators"
+        role="button"
+        data-slide="next"
+      >
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+  );
 }
 
 export default Projects;
