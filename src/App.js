e2b5605acf1849dfe5d669fe0d25c170b4d4a73f
Mark <markloercher@gmail.com>
2022-03-10 13:37:51 -0500

    askbcs try

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ function App() {
         {/* Routes = Switch in react router v6 */}
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/ml-react-portfolio" element={<Navigate to="/" />} />
+          <Route path="/ml-react-portfolio" element={<Home />} />
           <Route exact path="/about" element={<About />} />
           <Route exact path="/projects" element={<Projects />} />
           <Route exact path="/contact" element={<Contact />} />
