36d40491c88cd9fbb40e4cbbe9df9fdf71b98ba4
Mark <markloercher@gmail.com>
2022-05-20 14:12:19 -0400

    added download link to contact

--- src/components/Contact/Contact2.js
+++ src/components/Contact/Contact2.js
@@ -1,12 +1,27 @@
-import React, { useState } from "react";
-import { validateEmail } from "../../utils/helpers";
+
 import "./index.css";
+import resume from '../../assets/images/MarkLoercherResume.pdf'
+
+
 function Contact() {
  
   return (
     <section>
       <h1>THANKS FOR STOPPING BY!</h1>
-     
+        <ul>
+            <li>
+                EMAIL: MarkLoercher@gmail.com
+            </li>
+            <li>
+                GITHUB: mloercher
+            </li>
+            <li>
+                EMAIL: MarkLoercher@gmail.com
+            </li>
+            <li>
+                <a href={resume} download>Download Here</a>RESUME
+            </li>
+        </ul>
     </section>
   );
 }
