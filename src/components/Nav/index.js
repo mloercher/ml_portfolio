0cd2553cb6a492be27cce1a0790fd1744b563935
Mark <markloercher@gmail.com>
2022-05-21 19:48:10 -0400

    added responsive navbar

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,70 +1,91 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.css";
 import { NavLink } from "react-router-dom";
 
+
 function Nav(props) {
+  const [click, setClick] = useState(false)
+
+  const handleClick = () => {
+    setClick(!click)
+  }
+
+  const closeMobileMenu = () => {
+    setClick(!click)
+  }
 
   return (
-    <nav className="flex-row px-1" id="container">
-      <ul className="flex-row" id="navItems">
-        <li>
-          <NavLink
-            to="/"
-            style={{ textDecoration: "none" }}
-            className={(navData) => (navData.isActive ? "active" : "inactive")}
-            onClick={() => {
-              props.setBackgroundColor("#C2D897");
-              props.setNavColor("#C2D897");
-              props.setFooterColor("#C2D897");
-            }}
-          >
-            HOME
-          </NavLink>
-        </li>
-        <li className="mx-2">
-          <NavLink
-            to="/about"
-            style={{ textDecoration: "none" }}
-            className={({ isActive }) => (isActive ? "active" : "inactive")}
-            onClick={() => {
-              props.setBackgroundColor("#EEF4D4");
-              props.setNavColor("#EEF4D4");
-              props.setFooterColor("#EEF4D4");
-            }}
-          >
-            ABOUT
-          </NavLink>
-        </li>
-        <li className="mx-2">
-          <NavLink
-            to="/projects"
-            style={{ textDecoration: "none" }}
-            className={({ isActive }) => (isActive ? "active" : "inactive")}
-            onClick={() => {
-              props.setBackgroundColor("#C2D897");
-              props.setNavColor("#C2D897");
-              props.setFooterColor("#C2D897");
-            }}
-          >
-            PROJECTS
-          </NavLink>
-        </li>
-        <li className="mx-2">
-          <NavLink
-            to="/contact"
-            style={{ textDecoration: "none" }}
-            className={({ isActive }) => (isActive ? "active" : "inactive")}
-            onClick={() => {
-              props.setBackgroundColor("#EEF4D4");
-              props.setNavColor("#EEF4D4");
-              props.setFooterColor("#EEF4D4");
-            }}
-          >
-            INFO
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <nav className="navbar">
+        <div className='navbar-container'>
+          <div className='menu-icon' >
+            <i className={click ? 'fas fa-times' : 'fas fa-bars'} onClick={handleClick} />
+          </div>
+          <ul className={click ? 'nav-menu active' : 'nav-menu'} >
+            <li className="nav-item">
+              <NavLink
+                to="/"
+                style={{ textDecoration: "none" }}
+                activeclassname='active' className='nav-links'
+                onClick={() => {
+                  props.setBackgroundColor("#EEF4D4");
+                  props.setNavColor("#EEF4D4");
+                  props.setFooterColor("#EEF4D4");
+                  closeMobileMenu();
+                }}
+              >
+                HOME
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                to="/about"
+                style={{ textDecoration: "none" }}
+                activeclassname='active' className='nav-links'
+                onClick={() => {
+                  props.setBackgroundColor("#EEF4D4");
+                  props.setNavColor("#EEF4D4");
+                  props.setFooterColor("#EEF4D4");
+                  closeMobileMenu();
+                }}
+              >
+                ABOUT
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                to="/projects"
+                style={{ textDecoration: "none" }}
+                activeclassname='active' className='nav-links'
+                onClick={() => {
+                  props.setBackgroundColor("#C2D897");
+                  props.setNavColor("#C2D897");
+                  props.setFooterColor("#C2D897");
+                  closeMobileMenu();
+                }}
+              >
+                PROJECTS
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                to="/contact"
+                style={{ textDecoration: "none" }}
+                activeclassname='active' className='nav-links'
+                onClick={() => {
+                  props.setBackgroundColor("#EEF4D4");
+                  props.setNavColor("#EEF4D4");
+                  props.setFooterColor("#EEF4D4");
+                  closeMobileMenu();
+                }}
+              >
+                INFO
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
   );
 }
 
