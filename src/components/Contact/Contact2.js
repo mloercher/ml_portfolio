829aded92d567dddb53bbe39268097743b78e95a
Mark <markloercher@gmail.com>
2022-08-29 19:27:33 -0400

    footer height for mobile

--- src/components/Contact/Contact2.js
+++ src/components/Contact/Contact2.js
@@ -13,12 +13,12 @@ function Contact() {
       <ul className="cont-container">
         <div className="email-git">
         <li>
-          <h2>EMAIL</h2>
-          <p><a href="mailto:markloercher@gmail.com">markloercher@gmail.com</a></p>
+          <h2 className="connect-title">EMAIL</h2>
+          <p className="links"><a href="mailto:markloercher@gmail.com">markloercher@gmail.com</a></p>
         </li>
         <li>
-          <h2>GITHUB</h2>
-          <p><a
+          <h2 className="connect-title">GITHUB</h2>
+          <p className="links"><a
             href="https://www.github.com/mloercher"
             target="_blank"
             without
@@ -27,8 +27,8 @@ function Contact() {
         </li>
         </div>
         <li className="res">
-          <h2>RESUME</h2>
-          <p><a href={resume} download>Download here ☺</a></p>
+          <h2 className="connect-title">RESUME</h2>
+          <p className="links"><a href={resume} download>Download here ☺</a></p>
         </li>
       </ul>
     </div>
